2008bf50481df49f03bdc6d789a3de18540d6576
雲風 <wangkanghk@gmail.com>
2025-07-02 17:01:36 +1000

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,31 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { Hero } from './components/Hero';
 import { Services } from './components/Services';
 import { About } from './components/About';
 import { Contact } from './components/Contact';
 import { ParticleNetwork } from './components/ParticleNetwork';
+import { BPPage } from './components/BPPage'; // Import the new BPPage component
 
 function App() {
   return (
-    <div className="relative">
-      <ParticleNetwork />
-      <Layout>
-        <Hero />
-        <Services />
-        <About />
-        <Contact />
-      </Layout>
-    </div>
+    <Router>
+      <div className="relative">
+        <ParticleNetwork />
+        <Routes>
+          <Route path="/" element={
+            <Layout>
+              <Hero />
+              <Services />
+              <About />
+              <Contact />
+            </Layout>
+          } />
+          <Route path="/bp" element={<BPPage />} /> {/* New route for BPPage */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
